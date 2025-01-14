e7656602ecdf66e5f4d4ccdb33208115ceeb4f17
laurie <1847238294@qq.com>
2025-01-14 16:38:48 +0800

    完成第一部分

--- cachelab-handout/csim.c
+++ cachelab-handout/csim.c
@@ -36,15 +36,15 @@ int main(int argc, char *argv[]) {
         case 's':
             s = atoi(optarg);
             S = 1 << s;
-            printf("%d\n", s);
+            // printf("%d\n", s);
             break;
         case 'E':
             E = atoi(optarg);
-            printf("%d\n", E);
+            // printf("%d\n", E);
             break;
         case 'b':
             b = atoi(optarg);
-            printf("%d\n", b);
+            // printf("%d\n", b);
             break;
         case 't':
             tracefile = optarg;
@@ -66,6 +66,7 @@ int main(int argc, char *argv[]) {
         if (op == 'I') continue; /* 忽略指令加载 */
         simulate_cache(cache, op, address, b, s, E, verbose, &hits, &misses, &evictions, time, size);
     }
+    printSummary(hits,misses,evictions);
     fclose(file);
     free(cache);
 
@@ -74,43 +75,46 @@ int main(int argc, char *argv[]) {
 
 void simulate_cache(line *cache, char op, unsigned long address, int b, int s, int E, int verbose,
                     int *hits, int *misses, int *evictions, int time, int size) {
-    int s_ = address >> b & ((1 << s) - 1);
+    int s_ = address >> b & ((1 << (s)) - 1);
     int tag_ = address >> (b + s);
     switch (op) {
+    case 'S':
     case 'L': {
         char judge = 1;
         for (int i = 0; i < E; i++) {
-            if (!cache[s_ + i].time) {
+            if (!cache[s_*E + i].time) {              //遇到空位就存，因为他是从小到大存，所以不会有问题
                 (*misses)++;
-                cache[s_ + i].tag = tag_;
-                cache[s_ + i].time = time;
+                cache[s_*E + i].tag = tag_;
+                cache[s_*E + i].time = time;
                 judge = 0;
                 if (verbose) {
                     printf("%c %lx,%d miss\n", op, address, size);
                 }
                 break;
             }
-            if (cache[s_ + i].tag == tag_) {
+            if (cache[s_*E + i].tag == tag_) {       //命中
                 (*hits)++;
                 judge = 0;
-                cache[s_ + i].time = time;
+                cache[s_*E + i].time = time;
                 if (verbose) {
                     printf("%c %lx,%d hit\n", op, address, size);
                 }
+                break;
             }
         }
         if (judge) { /* 缓存未命中 */
             (*misses)++;
-            int record = cache[s_].time;
+            (*evictions)++;
+            int record = cache[s_*E].time;
             int change = 0;
             for (int i = 1; i < E; i++) {
-                if (cache[s_ + i].time < record) {
+                if (cache[s_*E + i].time < record) {
                     change = i;
-                    record = cache[s_ + i].time;
+                    record = cache[s_ *E+ i].time;
                 }
             }
-            cache[s_ + change].time = time;
-            cache[s_ + change].tag = tag_;
+            cache[s_*E + change].time = time;
+            cache[s_*E + change].tag = tag_;
             if (verbose) {
                 printf("%c %lx,%d miss eviction\n", op, address, size);
             }
@@ -118,16 +122,57 @@ void simulate_cache(line *cache, char op, unsigned long address, int b, int s, i
         break;
     }
     case 'M':
-        break;
-
-    case 'S':
-        break;
+        {
+            char judge = 1;
+            for (int i = 0; i < E; i++) {
+                if (!cache[s_*E + i].time) {              //遇到空位就存，因为他是从小到大存，所以不会有问题
+                    (*misses)++;
+                    (*hits)++;
+                    cache[s_*E + i].tag = tag_;
+                    cache[s_*E + i].time = time;
+                    judge = 0;
+                    if (verbose) {
+                        printf("%c %lx,%d miss hit\n", op, address, size);
+                    }
+                    break;
+                }
+                if (cache[s_*E + i].tag == tag_) {       //命中
+                    (*hits)++;
+                    (*hits)++;
+                    judge = 0;
+                    cache[s_*E + i].time = time;
+                    if (verbose) {
+                        printf("%c %lx,%d hit hit\n", op, address, size);
+                    }
+                    break;
+                }
+            }
+            if (judge) { /* 缓存未命中 */
+                (*misses)++;
+                (*evictions)++;
+                (*hits)++;
+                int record = cache[s_*E].time;
+                int change = 0;
+                for (int i = 1; i < E; i++) {
+                    if (cache[s_*E + i].time < record) {
+                        change = i;
+                        record = cache[s_*E + i].time;
+                    }
+                }
+                cache[s_*E + change].time = time;
+                cache[s_*E + change].tag = tag_;
+                if (verbose) {
+                    printf("%c %lx,%d miss eviction hit\n", op, address, size);
+                }
+            }
+            break;
+        }
     }
 }
 
-void printSummary(int hits, int misses, int evictions) {
-    printf("hits:%d misses:%d evictions:%d\n", hits, misses, evictions);
-}
+// void printSummary(int hits, int misses, int evictions) {
+//     printf("hits:%d misses:%d evictions:%d\n", hits, misses, evictions);
+// }
 
 void print_usage() {
     printf("Usage: ./csim-ref [-hv] -s <s> -E <E> -b <b> -t <tracefile>\n");
